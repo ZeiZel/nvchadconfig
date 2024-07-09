b7df17cbabaf9d54216df72b03f79175144f2136
ZeiZel <valera2003lvov@yandex.ru>
2024-07-09 21:15:56 +0300

    added needed lsp / configured plugins, mappings and much more

---
+++ lua/configs/lint.lua
@@ -0,0 +1,14 @@
+-- eslint enabled by daemon for more speed
+
+require("lint").linters_by_ft = {
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,241 @@
 return {
+  -- plugin for formatting files
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     config = function()
       require "configs.conform"
     end,
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
+  -- more easier navigation between tmux and nvim
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+  },
+  -- beautiful edits in place (project tree `r` and `a`)
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+  -- config for language servers
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  -- tests for nvim
+  { "nvim-neotest/nvim-nio" },
+  -- default installed lsp's
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "gopls",
+        "js-debug-adapter",
+        "typescript-language-server",
+      },
+    },
+  },
+  -- syntax highlighter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "typescript",
+        "javascript",
+        "go",
+      },
+    },
+  },
+  -- nvim linter
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "configs.lint"
+    end,
+  },
+  -- 
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  -- 
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  -- 
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  -- 
   -- {
-  --   "neovim/nvim-lspconfig",
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
   --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
+  --     require("chatgpt").setup {}
   --   end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
   -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- testing plugin for nvim
+  {
+    "nvim-neotest/neotest",
+    event = "VeryLazy",
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-jest" {
+            jestCommand = "npm test --",
+            jestConfigFile = "jest.config.ts",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          },
+        },
+      }
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "haydenmeade/neotest-jest",
+    },
+  },
+  -- 
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local ok, dap = pcall(require, "dap")
+      if not ok then
+        return
+      end
+      dap.configurations.typescript = {
+        {
+          type = "node2",
+          name = "node attach",
+          request = "attach",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+        },
+      }
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node-debug2-adapter",
+        args = {},
+      }
+    end,
+    dependencies = {
+      "mxsdev/nvim-dap-vscode-js",
+    },
+  },
+  -- 
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+
+      local dap, dapui = require "dap", require "dapui"
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close {}
+      end
+    end,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+  },
+  -- 
+  {
+    "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup {
+        library = { plugins = { "nvim-dap-ui" }, types = true },
+      }
+    end,
+  },
+  -- 
+  { "tpope/vim-fugitive" },
+  -- 
+  { "rbong/vim-flog", dependencies = {
+    "tpope/vim-fugitive",
+  }, lazy = false },
+  -- 
+  { "sindrets/diffview.nvim", lazy = false },
+  -- enable fast search by `s` + two symbols 
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      require("leap").add_default_mappings(true)
+    end,
+  },
+  -- 
+  {
+    "kevinhwang91/nvim-bqf",
+    lazy = false,
+  },
+  -- helful error shower 
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  -- defining todos 
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      require("todo-comments").setup()
+    end,
+  }, 
+  -- To make a plugin not be loaded
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+  },
 }
